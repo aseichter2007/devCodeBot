3f89249aa0a8415f4b2975ffc80a9b2cac2991a3
aseichter2007 <aseichter2007@gmail.com>
2020-06-10 08:23:56 -0500

    started terrible, ugly logic in post method to handle everything. Needs to be split into methods. defined our json format

--- SearchHelperBot/SearchHelperBot/Controllers/ValuesController.cs
+++ SearchHelperBot/SearchHelperBot/Controllers/ValuesController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Repository.Contracts;
+using Repository.Models;
+using SearchHelperBot.Model;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -39,10 +41,106 @@ namespace SearchHelperBot.Controllers
         
         
         // POST api/<ValuesController>
+        //post will be our sole entrypoint and the incoming file will define what happens
         [HttpPost]
-        public void Post([FromBody] string value)
+        public async Task<Outgoing> Post([FromBody] Incoming incoming)//this is looking incredibly ugly and should probably break into a bunch of methods or go into its own class like I did DbHandler
         {
+            if (incoming.search.role=="student")//check if incoming message is a basic search
+            {
+                int day = incoming.search.request.day;
+                string search = incoming.search.request.search;
+                Outgoing searchresult = new Outgoing();//the outgoing object contains lists of each kind of model and a List<string> for the search results, so that I can create a selectlist on the front side for them to choose what to edit or delete
+                searchresult.Searches= await ProcessSearch(day, search);
+                return searchresult;
+            }
+            else//if not type student, it must be an instructor
+            {
+                if (incoming.search.request.type == "badwords")//check request type and return a list of that type
+                {
+                    Outgoing badwordslist = new Outgoing();
+                    badwordslist.Responsetype = "badwords";
+                    badwordslist.badWords = _repo.BadWord.FindAll().ToList();
+                }
+                else if (incoming.search.request.type == "badphrases")
+                {
+                    //return list of badPhrases
+                }
+                //continue and serve each list type
+                else if (incoming.search.request.type == "add")//request type can also be crud operations
+                {
+                    if (incoming.search.add.type=="badword")//the search.add.type tells you what type has been requested to be added
+                    {
+                        BadWord badWord = new BadWord();
+                        badWord.Word = incoming.search.add.name;// I badwords and phrases just have a name or phrase. the id will be generated
+                        _repo.BadWord.Create(badWord);
+                        Outgoing outgoing = new Outgoing();//respond with a list of all badwords for review
+                        outgoing.Responsetype = "badwords";
+                        outgoing.badWords = _repo.BadWord.FindAll().ToList();
+                        return outgoing;
+                    }
+                    else if (incoming.search.add.type == "badphrase")//I am kind of slopy on my cases, whether all lowercase or camel case is is best use in the json, if you decide it should be a particular way, let me know, we just need consistency
+                    {
+                        BadPhrase badPhrase = new BadPhrase();
+                        badPhrase.Phrase = incoming.search.add.name;
+                        _repo.BadPhrase.Create(badPhrase);
+                        Outgoing outgoing = new Outgoing();
+                        outgoing.Responsetype = "badphrases";
+                        outgoing.badPhrases = _repo.BadPhrase.FindAll().ToList();
+                        return outgoing;
+                    }
+                    if (incoming.search.add.type == "nearconceptphrase")//this one is the trickiest cause you need to link the right NearConceptIdea, only if it exists
+                    {
+                        NearConceptIdea nearConceptIdea = _repo.NearConceptIdea.FindByCondition(c => c.ProperForm == incoming.search.add.matchTo).SingleOrDefault();//until I am more sure how complex the modals will let things become, shy away from the ids for finding things
+                        NearConceptPhrase nearConceptPhrase = new NearConceptPhrase();
+                        if (nearConceptIdea == null)//not sure how this repo system will return. if it's empty we need to populate its values and add it to the database, then pull it again to get the key to pair the foreign key. 
+                        {
+                            nearConceptPhrase.Phrase = incoming.search.add.name;
+                            nearConceptIdea.ProperForm = incoming.search.add.matchTo;//properform is the working string in nearconceptidea.  matchto will have the desired matched search.  I did a terrible job naming these things, sorry. Change them if you like but the keys have to match the json so I will need the exact changes. 
+                        }
 
+                    }
+                    //continue for all types that can be added. I dont think we are using Instructors.cs at this time but until we are sure, dont remove it
+
+
+
+
+
+
+
+                    Outgoing error3 = new Outgoing();
+                    error3.Responsetype = "something went wrong in add";
+                    return error3;
+                }
+                else if (incoming.search.request.type == "edit")// for request type edit, find the existing database entry, and update it with the incoming values. 
+                {
+                    if (incoming.search.edit.type == "badword")
+                    {
+                        BadWord badWord;//find in database and update values
+                    }
+                    else if (incoming.search.edit.type == "badphrase")
+                    {
+
+                    }
+                    //contine for all types
+
+
+
+                    Outgoing error3 = new Outgoing();
+                    error3.Responsetype = "something went wrong in edit";
+                    return error3;
+                }
+                else if (true)
+                {
+
+                }
+
+                Outgoing error2 = new Outgoing();
+                error2.Responsetype = "something went wrong in request check";
+                return error2;
+            }
+            Outgoing error = new Outgoing();
+            error.Responsetype = "something went wrong in role test";
+            return error;           
         }
 
         // PUT api/<ValuesController>/5
---
+++ SearchHelperBot/SearchHelperBot/Model/Outgoing.cs
@@ -0,0 +1,29 @@
+﻿using Repository.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SearchHelperBot.Model
+{
+    public class Outgoing
+    {
+        public string Responsetype;
+        public List<string> Searches;
+        public List<ActiveProject> activeProjects;
+        public List<PreferredLanguage> preferredLanguages;
+        public List<PreferredSearch> preferredSearches;
+        public List<Setting> settings;
+        public List<BadWord> badWords;
+        public List<BadPhrase> badPhrases;
+        public List<NearConceptIdea> conceptideas;
+        public List<NearConceptPhrase> conceptPhrases;
+        public List<Language> languages;
+        public List<Platform> platforms;
+        public List<RawSearch> rawSearches;
+        
+
+
+
+    }
+}
