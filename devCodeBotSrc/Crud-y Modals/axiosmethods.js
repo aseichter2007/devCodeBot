588785c8a3c3329c3b8c5a68a8ecfd86456cd503
aseichter2007 <aseichter2007@gmail.com>
2020-06-11 07:33:38 -0500

    finished axios post method, not sure if it works.

--- devCodeBotSrc/Crud-y Modals/axiosmethods.js
+++ devCodeBotSrc/Crud-y Modals/axiosmethods.js
@@ -1,6 +1,11 @@
-function AxiosGetRequest(output){
+function AxiosPostRequest(output){
+    
     const axios = require("axios");
- axios.axios.post("https://localhost:44317/api/values/" , output).then()
-
-
+ axios.axios.post("https://localhost:44317/api/values/" , output).then((response) => {
+     return response;//not sure if this is gonna work like I want in javascript. 
+    console.log(response);
+  }, (error) => {
+    console.log(error);
+  });
 }
+exports.AxiosPostRequest=AxiosPostRequest
